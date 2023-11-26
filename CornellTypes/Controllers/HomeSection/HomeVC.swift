39f57ceb69c899f80d126c7adaf41d8d95f5a952
Alvaro Deras <ad2226@cornell.edu>
2023-11-26 15:48:28 -0500

    Worked on UI for Homepage

--- CornellTypes/Controllers/HomeSection/HomeVC.swift
+++ CornellTypes/Controllers/HomeSection/HomeVC.swift
@@ -9,4 +9,128 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    // MARK: - Properties (view)
+    
+    private let logo = UIImageView()
+    private let exploreMBTITypes = UILabel()
+    private let pfp = UIButton()
+    private let mbtiStats = UIButton()
+    private let nameLabel = UILabel()
+    private let postButton = UIButton()
+    
+    // MARK: - Properties (data)
+    
+    private let posts: [Post] = []
+    private let profileImage: String = ""
+    
+    // MARK: - viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.hack.white
+        self.navigationItem.hidesBackButton = true
+        
+        setupLogo()
+        setupPFP()
+        setupExploreMBTITypes()
+        setupMBTIStats()
+        setupPostButton()
+    }
+    
+    // MARK: - Set Up Views
+    
+    private func setupLogo() {
+        logo.image = UIImage(named: "miniCornellTypesLogo")
+        
+        view.addSubview(logo)
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            logo.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            logo.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            logo.widthAnchor.constraint(equalToConstant: 170),
+            logo.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    private func setupPFP() {
+        pfp.setBackgroundImage(UIImage(named: "\(profileImage)"), for: .normal)
+        pfp.layer.cornerRadius = 33.025
+        pfp.layer.masksToBounds = true
+        pfp.addTarget(self, action: #selector(pushUserProfile), for: .touchUpInside)
+        
+        view.addSubview(pfp)
+        pfp.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pfp.centerYAnchor.constraint(equalTo: logo.centerYAnchor, constant: 15),
+            pfp.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 293),
+            pfp.widthAnchor.constraint(equalToConstant: 66.05),
+            pfp.heightAnchor.constraint(equalToConstant: 66.05)
+        ])
+    }
+    
+    private func setupExploreMBTITypes() {
+        exploreMBTITypes.text = "explore mbti types"
+        exploreMBTITypes.textColor = UIColor.hack.red
+        exploreMBTITypes.font = UIFont(name: "Fredoka-Regular", size: 22)
+        
+        view.addSubview(exploreMBTITypes)
+        exploreMBTITypes.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            exploreMBTITypes.topAnchor.constraint(equalTo: pfp.bottomAnchor, constant: 21.95),
+            exploreMBTITypes.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func setupMBTIStats() {
+        mbtiStats.setTitle("mbti stats", for: .normal)
+        mbtiStats.titleLabel!.font = UIFont(name: "Fredoka-Medium", size: 22)
+        mbtiStats.setTitleColor(UIColor.hack.white, for: .normal)
+        mbtiStats.setBackgroundImage(UIImage(named: "tanrect"), for: .normal)
+        mbtiStats.layer.cornerRadius = 16
+        mbtiStats.addTarget(self, action: #selector(pushStats), for: .touchUpInside)
+        
+        view.addSubview(mbtiStats)
+        mbtiStats.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mbtiStats.topAnchor.constraint(equalTo: exploreMBTITypes.bottomAnchor, constant: 118),
+            mbtiStats.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 108),
+            mbtiStats.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -108),
+            mbtiStats.widthAnchor.constraint(equalToConstant: 159),
+            mbtiStats.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func setupPostButton() {
+        postButton.setBackgroundImage(UIImage(named: "postbutton"), for: .normal)
+        postButton.layer.cornerRadius = 33.025
+        postButton.layer.masksToBounds = true
+        postButton.addTarget(self, action: #selector(pushPost), for: .touchUpInside)
+        
+        view.addSubview(postButton)
+        postButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            postButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20.95),
+            postButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 296),
+            postButton.widthAnchor.constraint(equalToConstant: 66.05),
+            postButton.heightAnchor.constraint(equalToConstant: 66.05)
+        ])
+    }
+    
+    @objc private func pushStats() {
+        
+    }
+    
+    @objc private func pushPost() {
+        
+    }
+    
+    @objc private func pushUserProfile() {
+        let vc = ProfileVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
